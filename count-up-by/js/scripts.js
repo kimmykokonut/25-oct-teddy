fd6515c451bf4c5861a11a4d21bbb0c2f6b75ed0
kimmykokonut <kimmykokonut@gmail.com>
2023-10-25 11:52:37 -0700

    complete count-up-by webpage with error cases
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- count-up-by/js/scripts.js
+++ count-up-by/js/scripts.js
@@ -2,15 +2,24 @@ function getResult(event) {
     event.preventDefault();
     const countTo = parseInt(document.querySelector("input#input1").value);
     const countBy = parseInt(document.querySelector("input#input2").value);
-    let result = [];
-    for (let num = countBy; num <= countTo; num += countBy) {
-        result.push(num)
-    }
     const div = document.querySelector("div#answer");
     const output = document.createElement("p");
     div.append(output);
-    output.innerText = "Output: " + result.join(", ");
-};
+    
+    if (countBy < 0 || countTo < 0) {
+        output.innerText = "Error, no negging allowed";
+    } else if (countTo < countBy) {
+        output.innerText = "Error, can't do that!!";
+    } else if (document.querySelector("input#input2").value === "" || document.querySelector("input#input1").value === "") {
+        output.innerText = "You didn't enter any values!";
+    } else {
+        let result = [];
+        for (let num = countBy; num <= countTo; num += countBy) {
+            result.push(num)
+        }
+        output.innerText = "Output: " + result.join(", ");
+        }   
+    };
 
 window.addEventListener("load", function () {
     const form = document.getElementById("count-form");
