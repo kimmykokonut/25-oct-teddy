1f307a498dc075533bfa19bf3440b5a08fab361d
kimmykokonut <kimmykokonut@gmail.com>
2023-10-25 15:14:02 -0700

    pass first test pig-latin pair project
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

---
+++ pig-latin/js/scripts.js
@@ -0,0 +1,9 @@
+const vowelArray = ["a", "e", "i", "o", "u"]
+function igPay(text) {
+        console.log(text[0]);
+        console.log(vowelArray);
+        if (vowelArray.includes(text[0])) {
+            return true;
+        }
+    }
+
