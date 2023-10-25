8a6f12b27785a5292cc1d9666a88a1d7f3f4f6e0
kimmykokonut <kimmykokonut@gmail.com>
2023-10-25 15:41:02 -0700

    pass test 3 and 4
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- pig-latin/js/scripts.js
+++ pig-latin/js/scripts.js
@@ -1,12 +1,14 @@
 const vowelArray = ["a", "e", "i", "o", "u"]
+const consonantArrayNoQ = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 function igPay(text) {
   if (vowelArray.includes(text[0])) {
     let result = text.concat("way");
-    return result
+    return result;
+  } else if (consonantArrayNoQ.includes(text[0])) {
+    let sliceText = text.slice(1);
+    let result = sliceText.concat(text[0]);
+    return result;
+  } else {
+    return false;
   }
-}
-
-
-
-// for slicing first char later
-// let sliceText = text.slice(1);
+  }
