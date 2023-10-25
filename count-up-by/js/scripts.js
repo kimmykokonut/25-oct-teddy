9725c600709954ea0e34cfb04bcb3c694e0364a3
kimmykokonut <kimmykokonut@gmail.com>
2023-10-25 11:24:25 -0700

    complete basic result for counting website
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- count-up-by/js/scripts.js
+++ count-up-by/js/scripts.js
@@ -1,16 +1,18 @@
-function getResult() {
-    result = [];
-    for (let num = countBy; num<= countTo; num += countBy) {
+function getResult(event) {
+    event.preventDefault();
+    const countTo = parseInt(document.querySelector("input#input1").value);
+    const countBy = parseInt(document.querySelector("input#input2").value);
+    let result = [];
+    for (let num = countBy; num <= countTo; num += countBy) {
         result.push(num)
-    } return result
+    }
+    const div = document.querySelector("div#answer");
+    const output = document.createElement("p");
+    div.append(output);
+    output.innerText = "Output: " + result.join(", ");
 };
 
-
-const countTo = parseInt(document.querySelector("input#input1").value);
-const countBy = parseInt(document.querySelector("input#input2").value);
-
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     const form = document.getElementById("count-form");
     form.addEventListener("submit", getResult);
-});
-
+    });
